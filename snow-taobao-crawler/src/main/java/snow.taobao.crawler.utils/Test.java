15aeb96b869c9c1bfddc4c3410312c12e1b4c046
Rzxuser <xuyunchong@1218.com.cn>
2018-12-20 20:43:42 +0800

    初始化

---
+++ snow-taobao-crawler/src/main/java/snow.taobao.crawler.utils/Test.java
@@ -0,0 +1,12 @@
+package snow.taobao.crawler.utils;
+
+/**
+ * @Author: xyc
+ * @Date: 2018/12/20 20:40
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        
+    }
+}
