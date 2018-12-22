a0a25fea921c11681b0c99fdec44d1e5bbab3e2f
xuyunchong <xuyunchong@1218.com.cn>
2018-12-22 17:34:17 +0800

    test

---
+++ snow-taobao-crawler/src/test/java/TaoBaoTest.java
@@ -0,0 +1,14 @@
+import us.codecraft.webmagic.processor.example.GithubRepoPageProcessor;
+
+/**
+ * @Author: xyc
+ * @Date: 2018/12/22 16:21
+ * @Version 1.0
+ */
+public class TaoBaoTest {
+
+    public static void main(String[] args) {
+        GithubRepoPageProcessor.main(args);
+    }
+
+}
---
+++ snow-taobao-crawler/src/test/java/WebmagicProcessor.java
@@ -0,0 +1,12 @@
+/**
+ * @Author: xyc
+ * @Date: 2018/12/22 17:13
+ * @Version 1.0
+ */
+public class WebmagicProcessor {
+
+    public static void main(String[] args) {
+
+    }
+
+}
